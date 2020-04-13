7d9710b863b25ead5a40f64a1fe31a27a06ca958
Kiichiro-T <tennis_0503@outlook.jp>
2020-04-13 14:39:32 +0900

    sendgriを本番で使えるようにする

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,13 +73,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    # user_name: ENV['SENDGRID_USERNAME'],
-    # password: ENV['SENDGRID_PASSWORD'],
-    user_name: ENV['DEV_EMAIL_USERNAME'],
-    password: ENV['DEV_EMAIL_PASSWORD'],
-    # domain: 'herokuapp.com',
-    # address: 'smtp.sendgrid.net',
-    address: 'smtp.gmail.com',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    # user_name: ENV['DEV_EMAIL_USERNAME'],
+    # password: ENV['DEV_EMAIL_PASSWORD'],
+    domain: 'herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    # address: 'smtp.gmail.com',
     port: 587,
     authentication: :plain,
     enable_starttls_auto: true
