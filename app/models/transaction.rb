6c366a77d1784e47023a211d7e8ff13a9724467d
Kiichiro-T <tennis_0503@outlook.jp>
2020-04-17 10:00:04 +0900

    Transactionモデルのアソシエーションの変更

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,9 +4,7 @@ class Transaction < ApplicationRecord
   attribute :url_token, :string, default: SecureRandom.hex(10)
   belongs_to :creditor, class_name: 'User', foreign_key: 'creditor_id'
   belongs_to :debtor, class_name: 'User', foreign_key: 'debtor_id'
-  belongs_to :event
-  belongs_to :group
-  validate  :deadline_before_today, on: :create
+  validate  :deadline_before_today, on: %i[create]
   validates :debt, presence: true
   validates :debt, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_blank: true
   validates :payment, presence: true
