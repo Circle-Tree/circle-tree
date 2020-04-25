1b3cacb6226d7725c8c904e7bbc4ff5d5e3bd6df
Kiichiro-T <tennis_0503@outlook.jp>
2020-04-25 14:26:19 +0900

    収支作成のリファクタリング

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -14,20 +14,7 @@ class AnswersController < ApplicationController
       @attending_answers = @event.answers.where(status: 'attending')
       flash_and_render(key: :danger, message: '回答を選択してください。', action: 'events/details')
     elsif @answer.save
-      user = User.find(@answer.user_id)
-      fee = Fee.find_by(event_id: @event.id, grade: User.find(user.id).grade)
-      transaction = Event::Transaction.find_by(event_id: @event.id, debtor_id: user.id)
-      if fee && @answer.reload.attending? && transaction.blank?
-        Event::Transaction.create(
-          deadline: fee.deadline,
-          debt: fee.amount,
-          payment: 0,
-          debtor_id: @answer.user_id,
-          creditor_id: fee.creditor_id,
-          event_id: @event.id,
-          url_token: SecureRandom.hex(10)
-        )
-      end
+      create_event_transaction(event: @event, answer: @answer)
       flash_and_redirect(key: :success, message: '回答を送信しました。', redirect_url: details_group_event_url(group_id: @event.group_id, id: @event.id))
     else
       @answer = nil
@@ -43,20 +30,7 @@ class AnswersController < ApplicationController
       answer = current_user.answers.find(params[:answer_id])
       event = answer&.event
       if answer&.update(status: params[:status])
-        user = User.find(answer.user_id)
-        fee = Fee.find_by(event_id: event.id, grade: user.grade)
-        transaction = Event::Transaction.find_by(event_id: event.id, debtor_id: user.id)
-        if answer.reload.attending? && fee && transaction.blank?
-          Event::Transaction.create(
-            deadline: fee.deadline,
-            debt: fee.amount,
-            payment: 0,
-            debtor_id: answer.user_id,
-            creditor_id: fee.creditor_id,
-            event_id: event.id,
-            url_token: SecureRandom.hex(10)
-          )
-        end
+        create_event_transaction(event: event, answer: answer)
         flash.now[:success] = '回答を変更しました'
       else
         ErrorSlackNotification.general_error_notify(title: '回答の変更に失敗', message: "#{current_user&.name}(ID: #{current_user&.id})さんがイベント(#{answer&.event&.id})の回答(#{answer&.id})に失敗")
@@ -71,20 +45,7 @@ class AnswersController < ApplicationController
 
   def update
     if @answer.update(update_answer_params)
-      user = User.find(@answer.user_id)
-      fee = Fee.find_by(event_id: @event.id, grade: user.grade)
-      transaction = Event::Transaction.find_by(event_id: @event.id, debtor_id: user.id)
-      if @answer.reload.attending? && fee && transaction.blank?
-        Event::Transaction.create(
-          deadline: fee.deadline,
-          debt: fee.amount,
-          payment: 0,
-          debtor_id: @answer.user_id,
-          creditor_id: fee.creditor_id,
-          event_id: @event.id,
-          url_token: SecureRandom.hex(10)
-        )
-      end
+      create_event_transaction(event: @event, answer: @answer)
       flash_and_redirect(key: :success, message: '回答を変更しました', redirect_url: details_group_event_url(group_id: @event.group_id, id: @event.id))
     else
       @group = @event.group
@@ -112,5 +73,22 @@ class AnswersController < ApplicationController
 
       flash[:danger] = 'アクセス権限がありません'
       raise Forbidden
+    end
+
+    def create_event_transaction(event:, answer:)
+      user = User.find(answer.user_id)
+      fee = Fee.find_by(event_id: event.id, grade: User.find(user.id).grade)
+      transaction = Event::Transaction.find_by(event_id: event.id, debtor_id: user.id)
+      if fee && answer.reload.attending? && transaction.blank?
+        Event::Transaction.create(
+          deadline: fee.deadline,
+          debt: fee.amount,
+          payment: 0,
+          debtor_id: answer.user_id,
+          creditor_id: fee.creditor_id,
+          event_id: event.id,
+          url_token: SecureRandom.hex(10)
+        )
+      end
     end
 end
