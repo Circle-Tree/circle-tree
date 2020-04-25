57fa86fb76639eb5b28cbbf8f83798207e07bed3
Kiichiro-T <tennis_0503@outlook.jp>
2020-04-25 16:46:26 +0900

    feeコントローラーのリファクタリング

--- app/controllers/fees_controller.rb
+++ app/controllers/fees_controller.rb
@@ -12,20 +12,7 @@ class FeesController < ApplicationController
   def create
     fee = @event.fees.build(fee_params)
     if fee.save
-      grade = fee.grade
-      members = User.members_by_grade(group: @group, grade: grade)
-      number = User.grades[grade.to_sym]
-      if members.any?
-        NewTransactionsJob.perform_later(members: members, event: @event, fee: fee, current_user: current_user)
-        if number.zero?
-          message = '出席するその他の人たちに通知しました。'
-        else
-          message = "出席する#{number}年生に通知しました。"
-        end
-        flash_and_redirect(key: :success, message: message, redirect_url: new_event_fee_url(event_id: @event.id))
-      else
-        flash_and_redirect(key: :success, message: "#{number}年生の支払い情報を保存しました。", redirect_url: new_event_fee_url(event_id: @event.id))
-      end
+      create_transaction(group: @group, event: @event, fee: fee)
     else
       @executives = User.executives(@group)
       flash_and_render(key: :danger, message: '間違いがあります。', action: 'new')
@@ -35,20 +22,7 @@ class FeesController < ApplicationController
   def update
     fee = @event.fees.find(params[:id])
     if fee.update(fee_params)
-      grade = fee.grade
-      members = User.members_by_grade(group: @group, grade: grade)
-      number = User.grades[grade.to_sym]
-      if members.any?
-        NewTransactionsJob.perform_later(members: members, event: @event, fee: fee, current_user: current_user)
-        if number.zero?
-          message = '出席するその他の人たちに通知しました。'
-        else
-          message = "出席する#{number}年生に通知しました。"
-        end
-        flash_and_redirect(key: :success, message: message, redirect_url: new_event_fee_url(event_id: @event.id))
-      else
-        flash_and_redirect(key: :success, message: "#{number}年生の支払い情報を保存しました。", redirect_url: new_event_fee_url(event_id: @event.id))
-      end
+      create_transaction(group: @group, event: @event, fee: fee)
     else
       @executives = User.executives(@group)
       flash_and_render(key: :danger, message: '間違いがあります。', action: 'new')
@@ -66,21 +40,19 @@ class FeesController < ApplicationController
       @group = @event.group
     end
 
-    # def change_fee(event:, grade:, transaction:)
-    #   fee = event.fees.find_by(grade: grade)
-    #   if fee.blank?
-    #     event.fees.create(
-    #       amount: transaction.debt,
-    #       grade: grade,
-    #       deadline: transaction.deadline,
-    #       creditor_id: transaction.creditor_id
-    #     )
-    #   else
-    #     fee.update(
-    #       amount: transaction.debt,
-    #       deadline: transaction.deadline,
-    #       creditor_id: transaction.creditor_id
-    #     )
-    #   end
-    # end
+    def create_transaction(group:, event:, fee:)
+      grade = fee.grade
+      members = User.members_by_grade(group: group, grade: grade)
+      number = User.grades[grade.to_sym]
+      if members.any?
+        NewTransactionsJob.perform_later(members: members, event: event, fee: fee, current_user: current_user)
+        flash_and_redirect(key: :success, message: flash_message(number), redirect_url: new_event_fee_url(event_id: event.id))
+      else
+        flash_and_redirect(key: :success, message: "#{number}年生の支払い情報を保存しました。", redirect_url: new_event_fee_url(event_id: event.id))
+      end
+    end
+
+    def flash_message(number)
+      number.zero? ? '出席するその他の人たちに通知しました。' : "出席する#{number}年生に通知しました。"
+    end
 end
