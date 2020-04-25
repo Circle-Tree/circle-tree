1a22e5637b9ef6d31389bf5bc07d08af2a33a05a
Kiichiro-T <tennis_0503@outlook.jp>
2020-04-25 18:10:07 +0900

    全学年のFeeを一括登録

--- app/controllers/fees_controller.rb
+++ app/controllers/fees_controller.rb
@@ -7,14 +7,16 @@ class FeesController < ApplicationController
 
   def new
     @executives = User.executives(@group)
+    @batch_fee = @event.fees.build
   end
 
   def create
     fee = @event.fees.build(fee_params)
     if fee.save
-      create_transaction(group: @group, event: @event, fee: fee)
+      create_or_update_transaction(group: @group, event: @event, fee: fee)
     else
       @executives = User.executives(@group)
+      @batch_fee = @event.fees.build
       flash_and_render(key: :danger, message: '間違いがあります。', action: 'new')
     end
   end
@@ -22,9 +24,24 @@ class FeesController < ApplicationController
   def update
     fee = @event.fees.find(params[:id])
     if fee.update(fee_params)
-      create_transaction(group: @group, event: @event, fee: fee)
+      create_or_update_transaction(group: @group, event: @event, fee: fee)
     else
       @executives = User.executives(@group)
+      @batch_fee = @event.fees.build
+      flash_and_render(key: :danger, message: '間違いがあります。', action: 'new')
+    end
+  end
+
+  def batch
+    @batch_fee = @event.fees.build(fee_params)
+    @batch_fee.valid?
+    @batch_fee.errors.messages.delete(:grade)
+    if @batch_fee.errors.messages.blank?
+      fee = create_or_update_fee(event: @event, batch_fee: @batch_fee)
+      batch_create_or_update_transaction(group: @group, event: @event, fee: fee)
+    else
+      @executives = User.executives(@group)
+      @batch_fee = @event.fees.build
       flash_and_render(key: :danger, message: '間違いがあります。', action: 'new')
     end
   end
@@ -40,19 +57,42 @@ class FeesController < ApplicationController
       @group = @event.group
     end
 
-    def create_transaction(group:, event:, fee:)
+    def create_or_update_transaction(group:, event:, fee:)
       grade = fee.grade
       members = User.members_by_grade(group: group, grade: grade)
+      NewTransactionsJob.perform_later(members: members, event: event, fee: fee, current_user: current_user) if members.any?
       number = User.grades[grade.to_sym]
-      if members.any?
-        NewTransactionsJob.perform_later(members: members, event: event, fee: fee, current_user: current_user)
-        flash_and_redirect(key: :success, message: flash_message(number), redirect_url: new_event_fee_url(event_id: event.id))
-      else
-        flash_and_redirect(key: :success, message: "#{number}年生の支払い情報を保存しました。", redirect_url: new_event_fee_url(event_id: event.id))
-      end
+      flash_and_redirect(key: :success, message: "#{number}年生の支払い情報を保存しました。", redirect_url: new_event_fee_url(event_id: event.id))
     end
 
-    def flash_message(number)
-      number.zero? ? '出席するその他の人たちに通知しました。' : "出席する#{number}年生に通知しました。"
+    def batch_create_or_update_transaction(group:, event:, fee:)
+      members = User.members(group)
+      NewTransactionsJob.perform_later(members: members, event: event, fee: fee, current_user: current_user) if members.any?
+      flash_and_redirect(key: :success, message: '全学年の支払い情報を一括保存しました。', redirect_url: new_event_fee_url(event_id: event.id))
     end
+
+    def create_or_update_fee(event:, batch_fee:)
+      Fee.grades.count.times do |i|
+        fee = event.fees.find_by(grade: i)
+        if fee.blank?
+          event.create(
+            amount: batch_fee.amount,
+            deadline: batch_fee.deadline,
+            creditor_id: batch_fee.creditor_id,
+            grade: i
+          )
+        else
+          fee.update(
+            amount: batch_fee.amount,
+            deadline: batch_fee.deadline,
+            creditor_id: batch_fee.creditor_id
+          )
+        end
+      end
+      event.fees.first
+    end
+
+    # def flash_message(number)
+    #   number.zero? ? '出席するその他の人たちに通知しました。' : "出席する#{number}年生に通知しました。"
+    # end
 end
