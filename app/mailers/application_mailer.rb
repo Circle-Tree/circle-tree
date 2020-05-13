99cbc07451d6c38f34762403182774b760d3bf14
Kiichiro-T <tennis_0503@outlook.jp>
2020-05-13 16:19:45 +0900

    送信元のアドレスをnorereplyに変更

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'kiichirotoyoizumi0221@gmail.com'
+  if ENV['RAILS_ENV'] == 'production'
+    default from: 'noreply@circle-tree.com'
+  else
+    default from: 'kiichirotoyoizumi0221@gmail.com'
+  end
   layout 'mailer'
 end
