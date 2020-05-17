631e6092778b70bebf201bc3fafdfa665af61550
Kiichiro-T <tennis_0503@outlook.jp>
2020-05-17 15:28:17 +0900

    管理者用アンケート作成ページ

---
+++ app/controllers/admin/questionnaires_controller.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Admin::QuestionnairesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :confirm_definitive_registration
+  before_action :non_admin_user_cannot_access
+
+  def index
+    all_questionnaires = Questionnaire.all
+    @admin_questionnaires = current_user.questionnaires
+    @non_admin_questionnaires = all_questionnaires - @admin_questionnaires
+  end
+
+  def new
+    @questionnaire = Questionnaire.new
+  end
+
+  def create
+    @questionnaire = Questionnaire.new(questionnaires_params)
+    if @questionnaire.save
+      flash_and_redirect(key: :success, message: 'アンケート作成！', redirect_url: admin_questionnaires_url)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def questionnaires_params
+      params.require(:questionnaire).permit(:title, :content, :user_id)
+    end
+end
