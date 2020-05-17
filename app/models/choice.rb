a40c36e2652a97e6b257f36b9775a84b0dcb9a57
Kiichiro-T <tennis_0503@outlook.jp>
2020-05-17 16:46:27 +0900

    選択肢が空白をバリデーション

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -4,6 +4,6 @@ class Choice < ApplicationRecord
   has_many :chooses, dependent: :destroy
   has_many :questionnaires, through: :chooses
 
-  validates :content, presence: false
+  validates :content, presence: true
   validates :content, length: { maximum: 32 }, allow_blank: true
 end
