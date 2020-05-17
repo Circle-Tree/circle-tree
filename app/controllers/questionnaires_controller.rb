1369405d9c8d5016954be954444cfe22f8abf0a7
Kiichiro-T <tennis_0503@outlook.jp>
2020-05-17 23:02:28 +0900

    作成直後のアンケートにはアクセスできない

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -3,6 +3,7 @@
 class QuestionnairesController < ApplicationController
   before_action :authenticate_user!
   before_action :confirm_definitive_registration
+  before_action :cannot_access_to_created_questionnaire, only: %i[show]
 
   def index
     @questionnaire = Questionnaire.new
@@ -28,5 +29,22 @@ class QuestionnairesController < ApplicationController
 
     def questionnaires_params
       params.require(:questionnaire).permit(:title, :content, :user_id)
+    end
+
+    def cannot_access_to_created_questionnaire
+      @questionnaire = Questionnaire.find(params[:id])
+      if @questionnaire.created?
+        flash_and_redirect(key: :danger, message: '無効なアンケートです。', redirect_url: questionnaires_url)
+      end
+    end
+
+    def set_form_path_and_response
+      @response = Response.find_by(user_id: current_user.id, questionnaire_id: @questionnaire.id)
+      if @response.blank?
+        @response = Response.new
+        @path = questionnaire_responses_path(questionnaire_id: @questionnaire.id)
+      else
+        @path = questionnaire_response_path(questionnaire_id: @questionnaire.id, id: @response)
+      end
     end
 end
