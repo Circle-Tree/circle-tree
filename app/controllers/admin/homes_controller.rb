7b2cd0f2936e1863abd7174bfc37e64ac71861b6
Kiichiro-T <tennis_0503@outlook.jp>
2020-05-17 16:30:27 +0900

    before_actionの追加

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,6 +2,7 @@
 
 class Admin::HomesController < ApplicationController
   before_action :authenticate_user!
+  before_action :confirm_definitive_registration
   before_action :non_admin_user_cannot_access
   def index
   end
