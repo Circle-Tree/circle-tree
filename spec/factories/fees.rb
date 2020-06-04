7e9a106b101ae5b6d241264ed2d94b0c2d6132fb
Kiichiro-T <tennis_0503@outlook.jp>
2020-06-05 03:40:15 +0900

    Feeモデルのテストを追加

---
+++ spec/factories/fees.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :fee do
+    amount { 1000 }
+    grade { Fee.grades[:grade1] }
+    deadline { Time.current.midnight.since(10.days) }
+    association :event
+    association :creditor, factory: :user
+
+    trait :invalid do
+      name { nil }
+    end
+  end
+end
