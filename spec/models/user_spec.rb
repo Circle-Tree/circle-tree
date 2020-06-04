4dc6340cef02e9bd04f5bec02102ceab24c214c6
Kiichiro-T <tennis_0503@outlook.jp>
2020-06-05 04:55:52 +0900

    Userのクラスメソッドを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -169,4 +169,153 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe 'クラスメソッド' do
+    context 'executives' do
+      let!(:group) { create(:group) }
+      it '正しい幹事たちを返す' do
+        executives = []
+        3.times do
+          executive = create(:user)
+          create(:group_user, :executive, group_id: group.id, user_id: executive.id)
+          executives << executive
+        end
+        5.times do
+          general = create(:user)
+          create(:general, group_id: group.id, user_id: general.id)
+        end
+        expect(User.executives(group)).to eq executives
+        expect(User.executives(group).count).to eq 3
+      end
+    end
+
+    context 'generals' do
+      let!(:group) { create(:group) }
+      it '正しい一般人たちを返す' do
+        generals = []
+        3.times do
+          executive = create(:user)
+          create(:group_user, :executive, group_id: group.id, user_id: executive.id)
+        end
+        5.times do
+          general = create(:user)
+          create(:general, group_id: group.id, user_id: general.id)
+          generals << general
+        end
+        expect(User.generals(group)).to eq generals
+        expect(User.generals(group).count).to eq 5
+      end
+    end
+
+    context 'members' do
+      let!(:group) { create(:group) }
+      it '正しいメンバーたちを返す' do
+        members = []
+        3.times do
+          executive = create(:user)
+          create(:group_user, :executive, group_id: group.id, user_id: executive.id)
+          members << executive
+        end
+        5.times do
+          general = create(:user)
+          create(:general, group_id: group.id, user_id: general.id)
+          members << general
+        end
+        expect(User.members(group)).to eq members
+        expect(User.members(group).count).to eq 8
+      end
+    end
+
+    context 'members_by_grade' do
+      let!(:group) { create(:group) }
+      it '正しい1年生たちを返す' do
+        members = []
+        3.times do
+          user = create(:user, grade: User.grades[:grade1])
+          create(:group_user, group_id: group.id, user_id: user.id)
+          members << user
+        end
+        5.times do
+          user = create(:user, grade: User.grades[:grade2])
+          create(:group_user, group_id: group.id, user_id: user.id)
+        end
+        expect(User.members_by_grade(group: group, grade: User.grades[:grade1])).to eq members
+        expect(User.members_by_grade(group: group, grade: User.grades[:grade1]).count).to eq 3
+      end
+    end
+
+    # 今度実装
+    # context 'uncompleted_transactions_and_members' do
+    # end
+
+    context 'return_gender_format' do
+      it '女ならばtrueを返す' do
+        str = '女'
+        expect(User.return_gender_format(str)).to be_truthy
+      end
+      it '男ならばfalseを返す' do
+        str = '男'
+        expect(User.return_gender_format(str)).to be_falsey
+      end
+      it '「女性」ならばnilを返す' do
+        str = '女性'
+        expect(User.return_gender_format(str)).to eq nil
+      end
+      it '「男性」ならばnilを返す' do
+        str = '男性'
+        expect(User.return_gender_format(str)).to eq nil
+      end
+      it '「あ」ならばnilを返す' do
+        str = 'あ'
+        expect(User.return_gender_format(str)).to eq nil
+      end
+      it '「a」ならばnilを返す' do
+        str = 'a'
+        expect(User.return_gender_format(str)).to eq nil
+      end
+      it '「1」ならばnilを返す' do
+        str = 1
+        expect(User.return_gender_format(str)).to eq nil
+      end
+    end
+
+    context 'return_grade_format' do
+      it '0ならば0を返す' do
+        str = '0'
+        expect(User.return_grade_format(str)).to eq 0
+      end
+      it '1ならば1を返す' do
+        str = '1'
+        expect(User.return_grade_format(str)).to eq 1
+      end
+      it '6ならば6を返す' do
+        str = '6'
+        expect(User.return_grade_format(str)).to eq 6
+      end
+      it '7ならばnilを返す' do
+        str = '7'
+        expect(User.return_grade_format(str)).to eq nil
+      end
+      it '-1ならばnilを返す' do
+        str = 'nil'
+        expect(User.return_grade_format(str)).to eq nil
+      end
+      it '「a1」ならばnilを返す' do
+        str = 'a1'
+        expect(User.return_gender_format(str)).to eq nil
+      end
+      # it '「1a」ならばnilを返す' do
+      #   str = '1a'
+      #   expect(User.return_gender_format(str)).to eq nil
+      # end
+      it '「あ」ならばnilを返す' do
+        str = 'あ'
+        expect(User.return_gender_format(str)).to eq nil
+      end
+      it '「a」ならばnilを返す' do
+        str = 'a'
+        expect(User.return_gender_format(str)).to eq nil
+      end
+    end
+  end
 end
