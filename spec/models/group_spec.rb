9475ba3a2aecd3567d248f604dbd6650475b2e39
Kiichiro-T <tennis_0503@outlook.jp>
2020-06-07 22:32:18 +0900

    Groupのインスタンスメソッドとクラスメソッドのテストを追加

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -76,4 +76,85 @@ RSpec.describe Group, type: :model do
     it { is_expected.to have_many(:events) }
     it { is_expected.to have_many(:transactions) }
   end
+
+  describe 'インスタンスメソッド' do
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+    let(:group1) { create(:group) }
+    let(:group2) { create(:group) }
+
+    context 'set_paid' do
+      it 'paidを返す' do
+        unpaid_group = create(:group, :unpaid)
+        expect(unpaid_group.set_paid).to eq Group.payment_statuses[:paid]
+      end
+    end
+
+    context 'my_group?' do
+      it 'trueを返す' do
+        create(:group_user, group_id: group1.id, user_id: user1.id)
+        expect(group1.my_group?(user1)).to be_truthy
+      end
+
+      it 'falseを返す' do
+        expect(group1.my_group?(user2)).to be_falsey
+      end
+    end
+
+    context 'my_own_group?' do
+      it 'trueを返す' do
+        create(:group_user, :executive, group_id: group2.id, user_id: user1.id)
+        expect(group2.my_own_group?(user1)).to be_truthy
+      end
+
+      it 'falseを返す' do
+        create(:group_user, group_id: group1.id, user_id: user1.id)
+        expect(group1.my_own_group?(user1)).to be_falsey
+      end
+    end
+  end
+
+  describe 'クラスタンスメソッド' do
+    let!(:group1) { create(:group) }
+    let!(:group2) { create(:group) }
+    let!(:group3) { create(:group) }
+    let!(:group4) { create(:group) }
+    let!(:user) { create(:user) }
+    let!(:group_user1) { create(:group_user, :executive, group_id: group1.id, user_id: user.id) }
+    let!(:group_user2) { create(:group_user, group_id: group2.id, user_id: user.id) }
+    let!(:group_user3) { create(:group_user, group_id: group3.id, user_id: user.id) }
+
+    context 'my_groups' do
+      it '自分の所属するサークルを返す' do
+        expect(Group.my_groups(user)).to eq [group1, group2, group3]
+      end
+      it '自分の所属しないサークルは返さない' do
+        expect(Group.my_groups(user)).to_not include group4
+      end
+    end
+
+    context 'my_own_groups' do
+      it '自分が幹事であるサークルを返す' do
+        expect(Group.my_own_group(user)).to eq group1
+      end
+      it '自分がメンバーであるサークルは返さない' do
+        expect(Group.my_own_group(user)).to_not eq group2
+      end
+      it '自分の所属しないサークルは返さない' do
+        expect(Group.my_own_group(user)).to_not eq group4
+      end
+    end
+
+    context 'my_general_groups' do
+      it '自分がメンバーであるサークルを返す' do
+        expect(Group.my_general_groups(user)).to eq [group2, group3]
+      end
+      it '自分が幹事であるサークルを返さない' do
+        expect(Group.my_general_groups(user)).to_not include group1
+      end
+      it '自分の所属しないサークルは返さない' do
+        expect(Group.my_general_groups(user)).to_not include group4
+      end
+    end
+  end
 end
